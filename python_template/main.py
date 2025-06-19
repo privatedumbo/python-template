dc61b80d5110656d2aab09fe974a326b11a9a612
franco-bocci <franco.bocci@wolt.com>
2025-06-19 19:13:03 +0200

    Initial commit

---
+++ python_template/main.py
@@ -0,0 +1,6 @@
+def foo() -> str:
+    return "bar"
+
+
+if __name__ == "__main__":
+    foo()
---
+++ tests/test_core.py
@@ -0,0 +1,5 @@
+from python_template.main import foo
+
+
+def test_foo() -> None:
+    assert foo() == "bar"
